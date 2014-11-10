4c1a204053571f6878f6102ee4922abcc136e9f7
Ben Hayden <benjhayden@google.com>
2014-11-10 14:06:08 -0800

    .

--- TinyDST/TinyDST.cpp
+++ TinyDST/TinyDST.cpp
@@ -1,31 +1,17 @@
-#include <avr/pgmspace.h>
-
-uint16_t dst_days[] PROGMEM = {
-  // DST begins on even-indexed elements, ends on odd-indexed elements
-  // elements are days since 2000 Jan 1.
-  5181, 5419,
-  5545, 5783,
-  5916, 6154,
-  6280, 6518,
-  6644, 6882,
-  7008, 7246,
-  7372, 7610,
-  7743, 7981,
-  8107, 8345,
-  8471, 8709,
-  8835, 9073,
-  9199, 9437,
-  9563, 9801,
-  9934, 10172,
-  10298, 10536,
-  10662, 10900,
-};
-
 boolean isDST(uint16_t day) {
-  for (uint8_t i = 0; i < ARRAYSIZE(dst_days); ++i) {
-    if (day < pgm_read_word(&(dst_days[i]))) {
-      return (i % 2) == 1;
-    }
+  if (day < 5181) return false;
+  day -= 5181;
+  // In the US, DST begins and ends on Sundays.
+  day /= 7;
+  for (uint16_t y = 0; y < 20; ++y) {
+    // DST summer time always lasts 34 weeks.
+    if (day < 34) return true;
+    day -= 34;
+    // standard winter time lasts 18 weeks most years except 2015, 2022, 2028.
+    uint16_t w = 18;
+    if ((y == 1) || (y == 8) || (y == 14)) ++w;
+    if (day < w) return false;
+    day -= w;
   }
   return false;
 }
--- TinyDST/TinyDST.h
+++ TinyDST/TinyDST.h
@@ -1,7 +1,8 @@
-// ATTINYs have very limited memory. Calculating whether a date is in DST from first principles (e.g. the Timezone library) can consume a lot of memory.
-// TinyDST calculates whether a date is in DST using as little memory as possible using a pre-computed table.
-
-// The pre-computed table will last until 2030. If you expect to still be using this library after that, then you'll need to re-compute the table.
+// ATTINYs have very limited flash program memory. Calculating whether a date is
+// in DST from first principles (e.g. the Timezone library) can consume a lot of
+// flash program memory.
+// TinyDST calculates whether a date is in DST using as little flash program
+// memory as possible.
 
 // Example usage:
 // https://github.com/benshayden/github/blob/master/lightclock/lightclock.ino
