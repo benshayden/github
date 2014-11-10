ed523af85f29a16fc210908543c3e7e6a9492291
benshayden <benshayden@users.noreply.github.com>
2014-11-10 11:51:28 -0800

    Create TinyDST.h

---
+++ TinyDST/TinyDST.h
@@ -0,0 +1,11 @@
+// Example usage:
+// https://github.com/benshayden/github/blob/master/lightclock/lightclock.ino
+
+#ifndef _TINYDST_H_
+#define _TINYDST_H_
+
+// Returns true if day (as the number of days since 2000 Jan 1)
+// is in Daylight Savings or Summer Time in the US.
+boolean isDST(uint16_t day);
+
+#endif  // _TINYDST_H_
