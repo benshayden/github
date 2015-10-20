a973b0b92b565e77cb888a34dda7973ad5d0057a
benshayden <benshayden@users.noreply.github.com>
2015-10-20 16:28:40 -0700

    Update keyboard.ino

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -13,9 +13,9 @@
 byte ROW_PINS[] = {0, 1, 2, 3, 4, 5};
 byte COLUMN_PINS[] = {16, 17, 18, 19, 20, 21};
 char KEYMAPS[][sizeof(ROW_PINS) * sizeof(COLUMN_PINS)] = {
-  // https://cdn.rawgit.com/benshayden/github/da6ea8e/keyboard/keymaps.html
-  {'z', 'b', 'm', 'h', KEY_BACKSPACE, 't', 'x', 'y', 'c', 'n', ' ', 'k', 'f', 'd', 'o', 'e', 'q', 'p', 'u', 's', KEY_ENTER, KEY_SHIFT, 'v', 'g', 'l', 'i', KEY_MODE, 'j', 'w', 'r', 'a', ';'},
-  {'`', '[', '\'', KEY_UP, '=', '-', '|', '/', KEY_LEFT, KEY_RIGHT, KEY_CTRL, '\\', ']', '.', KEY_DOWN, ',', KEY_VOLDN, '9', '8', '7', KEY_TAB, KEY_SHIFT, KEY_VOLUP, '6', '5', '4', KEY_MODE, KEY_MACRO, '3', '2', '1', '0'}
+  // https://cdn.rawgit.com/benshayden/github/60bead3/keyboard/keymaps.html
+  {'z', 'b', 'm', 'h', KEY_BACKSPACE, 't', 'x', 'y', 'c', 'n', ' ', 0, 'k', 'f', 'd', 'o', 'e', 0, 'q', 'p', 'u', 's', KEY_ENTER, KEY_SHIFT, 'v', 'g', 'l', 'i', KEY_MODE, 0, 'j', 'w', 'r', 'a', ';', 0},
+  {'`', '[', '\'', KEY_UP, '=', '-', '|', '/', KEY_LEFT, KEY_RIGHT, KEY_CTRL, 0, '\\', ']', '.', KEY_DOWN, ',', 0, KEY_VOLDN, '9', '8', '7', KEY_TAB, KEY_SHIFT, KEY_VOLUP, '6', '5', '4', KEY_MODE, 0, KEY_MACRO, '3', '2', '1', '0', 0}
 };
 
 // END SETTINGS
