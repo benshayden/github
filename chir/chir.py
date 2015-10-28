711f738a260a792423dc12a757c9aaee33d9d56f
benshayden <benshayden@users.noreply.github.com>
2015-10-28 13:42:49 -0700

    Update chir.py

--- chir/chir.py
+++ chir/chir.py
@@ -1,13 +1,3 @@
-# Computer-Human Interaction Research
-# This is a simple web app that asks the user to perform interactions
-# such as clicking, scrolling, or loading,
-# delays those interactions by a random amount of time,
-# asks the user whether the interaction was painful/comfortable/frustrating,
-# stores information about the interaction,
-# and presents an analysis of what percentage of users
-# found what flavors of interactions with what amounts of delay
-# to be painful/comfortable/frustrating.
-
 import webapp2
 from google.appengine.ext import ndb
 
