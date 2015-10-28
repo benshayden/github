dab40e56cfca82937194377b66d844faf60c12cd
benshayden <benshayden@users.noreply.github.com>
2015-10-28 16:42:26 -0700

    Update chir.py

--- chir/chir.py
+++ chir/chir.py
@@ -1,11 +1,25 @@
 import webapp2
 from google.appengine.ext import ndb
+from google.appengine.api import memcache
 
 class Interaction(ndb.Model):
   flavor = ndb.StringProperty(indexed=False)
   delayMs = ndb.IntegerProperty()
   wording = ndb.StringProperty(indexed=False)
   wasBad = ndb.BooleanProperty()
+  
+  @classmethod
+  def histogram(cls, wording, flavor):
+    histogram = {}
+    interactions = cls.query()
+    if wording:
+      interactions = interactions.filter(cls.wording == wording)
+    if flavor:
+      interactions = interactions.filter(cls.flavor == flavor)
+    for interaction in interactions:
+      bucket = histogram.setdefault(interaction.delayMs // 10, [0, 0])
+      bucket[interaction.wasBad] += 1
+    return histogram
 
 class Record(webapp2.RequestHandler):
   def post(self):
@@ -18,18 +32,15 @@ class Record(webapp2.RequestHandler):
 
 class Graph(webapp2.RequestHandler):
   def get(self):
-    histogram = {}
     wording = self.request.get('wording')
     flavor = self.request.get('flavor')
-    interactions = Interaction.query()
-    if wording:
-      interactions = interactions.filter(Interaction.wording == wording)
-    if flavor:
-      interactions = interactions.filter(Interaction.flavor == flavor)
-    for interaction in interactions:
-      bucket = histogram.setdefault(interaction.delayMs // 10, [0, 0])
-      bucket[interaction.wasBad] += 1
-    self.response.out.write(repr(histogram.items()))
+    
+    memcacheKey = repr((wording, flavor))
+    histogram = memcache.get(memcacheKey)
+    if histogram is None:
+      histogram = repr(Interaction.histogram(wording, flavor.items())
+      memcache.add(memcacheKey, histogram, time=3600)
+    self.response.out.write(histogram)
 
 app = webapp2.WSGIApplication([
   ('/record', Record),
