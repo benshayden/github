3386cbc5dada9f0dc5ec2a20637f905af3353e71
benshayden <benshayden@users.noreply.github.com>
2015-10-28 12:02:01 -0700

    Update keyboard.ino

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -13,7 +13,7 @@
 byte ROW_PINS[] = {0, 1, 2, 3, 4, 5};
 byte COLUMN_PINS[] = {16, 17, 18, 19, 20, 21};
 char KEYMAPS[][sizeof(ROW_PINS) * sizeof(COLUMN_PINS)] = {
-  // https://cdn.rawgit.com/benshayden/github/60bead3/keyboard/keymaps.html
+  // https://cdn.rawgit.com/benshayden/github/a9df19/keyboard/keymaps.html
   {'z', 'b', 'm', 'h', KEY_BACKSPACE, 't', 'x', 'y', 'c', 'n', ' ', 0, 'k', 'f', 'd', 'o', 'e', 0, 'q', 'p', 'u', 's', KEY_ENTER, KEY_SHIFT, 'v', 'g', 'l', 'i', KEY_MODE, 0, 'j', 'w', 'r', 'a', ';', 0},
   {'`', '[', '\'', KEY_UP, '=', '-', '|', '/', KEY_LEFT, KEY_RIGHT, KEY_CTRL, 0, '\\', ']', '.', KEY_DOWN, ',', 0, KEY_VOLDN, '9', '8', '7', KEY_TAB, KEY_SHIFT, KEY_VOLUP, '6', '5', '4', KEY_MODE, 0, KEY_MACRO, '3', '2', '1', '0', 0}
 };
