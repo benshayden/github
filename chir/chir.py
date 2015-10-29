bbeee3f1ff52e25346cd37a782a370a3cc14c072
benshayden <benshayden@users.noreply.github.com>
2015-10-29 11:55:12 -0700

    Update chir.py

--- chir/chir.py
+++ chir/chir.py
@@ -9,20 +9,31 @@ class Interaction(ndb.Model):
   slowness = ndb.IntegerProperty()
   
   @classmethod
-  def histogram(cls):
-    histogram = {}
+  def histograms(cls):
+    # build {'click': [total, {delayMs/10: [total, {slowness/10: count}]}]} 
+    histograms = {}
     interactions = cls.query()
     for interaction in interactions:
       if interaction.delayMs is None or interaction.slowness is None or interaction.interactionType is None:
         continue
-      subhist = histogram.setdefault(interaction.interactionType, {})
-      subhist = subhist.setdefault(interaction.delayMs // 10, {})
+      interactionHistogram = histograms.setdefault(interaction.interactionType, [0, {}])
+      interactionHistogram[0] += 1
+      slownessHistogram = interactionHistogram[1].setdefault(interaction.delayMs // 10, [0, {}])
+      slownessHistogram[0] += 1
+      slownessHistogram = slownessHistogram[1]
       bucket = interaction.slowness // 10
-      if bucket in subhist:
-        subhist[bucket] += 1
+      if bucket in slownessHistogram:
+        slownessHistogram[bucket] += 1
       else:
-        subhist[bucket] = 1
-    return histogram
+        slownessHistogram[bucket] = 1
+    # return {'click': [total, [/*sorted*/[delayMs/10, [total, [/*sorted*/[slowness/10, count]]]]]]}
+    for interactionType in histograms:
+      interactionHistogram = histograms[interactionType]
+      for delayBucket in interactionHistogram[1]:
+        slownessHistogram = interactionHistogram[1][delayBucket]
+        slownessHistogram[1] = sorted(slownessHistogram[1].items())
+      interactionHistogram[1] = sorted(interactionHistogram[1].items())
+    return histograms
 
 class Record(webapp2.RequestHandler):
   def post(self):
@@ -34,12 +45,12 @@ class Record(webapp2.RequestHandler):
 
 class Graph(webapp2.RequestHandler):
   def get(self):
-    memcacheKey = 'histogram'
-    histogram = memcache.get(memcacheKey)
-    if histogram is None:
-      histogram = json.dumps(Interaction.histogram())
-      memcache.add(memcacheKey, histogram, time=10)
-    self.response.out.write(histogram)
+    memcacheKey = 'histograms'
+    histograms = memcache.get(memcacheKey)
+    if histograms is None:
+      histograms = json.dumps(Interaction.histograms())
+      memcache.add(memcacheKey, histograms, time=10)
+    self.response.out.write(histograms)
 
 app = webapp2.WSGIApplication([
   ('/record', Record),
