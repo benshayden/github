d09be865150095a1a18bcbedcaaa60bd1fced17c
benshayden <benshayden@users.noreply.github.com>
2015-10-29 08:56:58 -0700

    Update chir.py

--- chir/chir.py
+++ chir/chir.py
@@ -5,20 +5,20 @@ from google.appengine.api import memcache
 class Interaction(ndb.Model):
   flavor = ndb.StringProperty(indexed=False)
   delayMs = ndb.IntegerProperty()
-  wording = ndb.StringProperty(indexed=False)
-  wasBad = ndb.BooleanProperty()
+  slowness = ndb.IntegerProperty()
   
   @classmethod
-  def histogram(cls, wording, flavor):
+  def histogram(cls):
     histogram = {}
     interactions = cls.query()
-    if wording:
-      interactions = interactions.filter(cls.wording == wording)
-    if flavor:
-      interactions = interactions.filter(cls.flavor == flavor)
     for interaction in interactions:
-      bucket = histogram.setdefault(interaction.delayMs // 10, [0, 0])
-      bucket[interaction.wasBad] += 1
+      subhist = histogram.setdefault(interaction.flavor, {})
+      subhist = subhist.setdefault(interaction.delayMs // 10, {})
+      bucket = interaction.slowness // 10
+      if bucket in subhist:
+        subhist[bucket] += 1
+      else:
+        subhist[bucket] = 1
     return histogram
 
 class Record(webapp2.RequestHandler):
@@ -26,19 +26,15 @@ class Record(webapp2.RequestHandler):
     interaction = Interaction(
       flavor=self.request.get('flavor'),
       delayMs=int(self.request.get('delayMs')),
-      wording=self.request.get('wording'),
-      wasBad=(self.request.get('wasBad') == 'true'))
+      slowness=int(self.request.get('slowness')))
     interaction.put()
 
 class Graph(webapp2.RequestHandler):
   def get(self):
-    wording = self.request.get('wording')
-    flavor = self.request.get('flavor')
-    
-    memcacheKey = repr((wording, flavor))
+    memcacheKey = 'histogram'
     histogram = memcache.get(memcacheKey)
     if histogram is None:
-      histogram = repr(Interaction.histogram(wording, flavor.items())
+      histogram = repr(Interaction.histogram().items())
       memcache.add(memcacheKey, histogram, time=3600)
     self.response.out.write(histogram)
 
