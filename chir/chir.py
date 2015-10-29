1612ac8a43d57c8f9e5929272511a95be4d7640a
benshayden <benshayden@users.noreply.github.com>
2015-10-29 12:29:40 -0700

    Update chir.py

--- chir/chir.py
+++ chir/chir.py
@@ -10,7 +10,10 @@ class Interaction(ndb.Model):
   
   @classmethod
   def histograms(cls):
-    # build {'click': [total, {delayMs/10: [total, {slowness/10: count}]}]} 
+    # returns [(interactionType, sampleCount, sorted[
+    #             (delayMs/10, sampleCount, sorted[
+    #               (slowness/10, count)])]]
+    # but first build {'click': [sampleCount, {delayMs/10: [sampleCount, {slowness/10: count}]}]} 
     histograms = {}
     interactions = cls.query()
     for interaction in interactions:
@@ -26,14 +29,18 @@ class Interaction(ndb.Model):
         slownessHistogram[bucket] += 1
       else:
         slownessHistogram[bucket] = 1
-    # return {'click': [total, [/*sorted*/[delayMs/10, [total, [/*sorted*/[slowness/10, count]]]]]]}
+
     for interactionType in histograms:
       interactionHistogram = histograms[interactionType]
       for delayBucket in interactionHistogram[1]:
         slownessHistogram = interactionHistogram[1][delayBucket]
         slownessHistogram[1] = sorted(slownessHistogram[1].items())
-      interactionHistogram[1] = sorted(interactionHistogram[1].items())
-    return histograms
+      interactionHistogram[1] = sorted([delayBucket, sampleCount, slownessHistogram]
+                                       for (delayBucket, (sampleCount, slownessHistogram))
+                                       in interactionHistogram[1].iteritems())
+    return [(interactionType, sampleCount, histogram)
+            for (interactionType, (sampleCount, histogram))
+            in histograms.iteritems()]
 
 class Record(webapp2.RequestHandler):
   def post(self):
